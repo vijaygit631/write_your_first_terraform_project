8c57f6a4a1534daa17cdf473bf25b6a5049e888a
vijaygit631 <vijaybabutirumalasetti@gmail.com>
2025-01-29 17:20:01 +0000

    fixed s3 version error

--- aws/remote_state/main.tf
+++ aws/remote_state/main.tf
@@ -33,13 +33,12 @@ locals {
 resource "aws_s3_bucket" "terraform_state" {
   # With account id, this S3 bucket names can be *globally* unique.
   bucket = "${local.account_id}-terraform-states"
+}
 
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+# Separate server-side encryption configuration
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
+  bucket = aws_s3_bucket.terraform_state.id
+
     }
   }
 }
