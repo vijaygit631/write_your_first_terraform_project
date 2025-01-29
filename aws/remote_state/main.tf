1d7b9b6d88b6d8bc8dfe130a8c43a549530bad94
vijaygit631 <vijaybabutirumalasetti@gmail.com>
2025-01-29 16:39:48 +0000

    added backend details

--- aws/remote_state/main.tf
+++ aws/remote_state/main.tf
@@ -62,3 +62,17 @@ resource "aws_dynamodb_table" "terraform_lock" {
     type = "S"
   }
 }
+
+#terraform backend
+
+terraform{
+  backend "s3"{
+    bucket = "THE_NAME_OF_THE_STATE_BUCKET"
+    key = "some_environment/terraform.tfstate"
+    region = "ap-south-1"
+    encrypt = true
+    kms_key_id = "THE_ID_OF_THE_KMS_KEY"
+    dynamodb_table = "THE_ID_OF_THE_DYNAMODB_TABLE"
+  } +
+}
+
